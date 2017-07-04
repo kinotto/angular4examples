8e5294abe6ce786a4df92d939137a8de8c8d9413
Karim <kinottohw@gmail.com>
2017-07-04 17:56:40 +0200

    form reactive approach (assignment 7)

---
+++ src/assignment7/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit} from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styles: [`
+    input.ng-touched.ng-invalid{
+      border-color: red;
+    }
+    label{
+      font-size: 13px;
+    }
+  `]
+})
+export class AppComponent implements OnInit{
+  myForm: FormGroup;
+
+  ngOnInit(){
+    //reactive approach
+    this.myForm = new FormGroup({
+      projectName: new FormControl('', [Validators.required
+      ], [this.validateProjectNameAsync]),
+      email: new FormControl('', Validators.email),
+      type: new FormControl('', Validators.required)
+    });
+  }
+
+  //sync validator
+  validatorProjectName(projectName: FormControl) {
+    if(projectName.value === 'Test'){
+      return {
+        projectName: {valid: false}
+      }
+    }
+    return null;
+  }
+
+  //async validator with Observables
+  validateProjectNameAsync(projectName: FormControl) :Observable<{any}> {
+    return new Observable((observer) => {
+      if(projectName.value === 'Test'){
+        observer.next({
+          projectName: false
+        })
+      } else{
+        observer.next(null);
+      }
+      observer.complete();
+    })
+    //.first();
+  }
+
+  //async validator with Promise
+  validateProjectNameAsync2(projectName: FormControl) :Promise<any> {
+    return new Promise((resolve, reject) => {
+      if(projectName.value === 'Test'){
+        return resolve({
+          projectName: false
+        })
+      } else{
+        return resolve(null);
+      }
+    })
+  }
+  onSubmit(){
+    console.log(this.myForm);
+  }
+
+}
